c4d1b6935f8166cf4c5e477efd61d6e80a18c9b9
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-06-13 23:03:22 +0200

    Update

--- footer.php
+++ footer.php
@@ -36,34 +36,34 @@ $template_root = get_template_directory_uri();
                     <!-- Begin MailChimp Signup Form -->
                     <link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
                     <style type="text/css">
-                        #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+                        #mc_embed_signup{background-color:transparent; clear:left; }
                         /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
                            We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
                     </style>
                     <div id="mc_embed_signup">
                         <form action="//strivecdn.us15.list-manage.com/subscribe/post?u=02ed6a6e43772f62f20d35008&amp;id=a9f47a9457" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
                             <div id="mc_embed_signup_scroll">
-                                <h2>Subscribe to our mailing list</h2>
-                                <div class="indicates-required"><span class="asterisk">*</span> indicates required</div>
+                                <h3 class="paragraph-headline">Subscribe to our mailing list</h3>
+                                <!-- <div class="indicates-required"><span class="asterisk">*</span> indicates required</div> -->
                                 <div class="mc-field-group">
-                                    <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
-                                    </label>
-                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+                                    <!-- <label for="mce-EMAIL">Email Address  <span class="asterisk">*</span>
+                                    </label> -->
+                                    <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL" placeholder="Emailadresse...">
                                 </div>
                                 <div class="mc-field-group">
-                                    <label for="mce-FNAME">First Name </label>
-                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME">
+                                    <!-- <label for="mce-FNAME">First Name </label> -->
+                                    <input type="text" value="" name="FNAME" class="" id="mce-FNAME" placeholder="Vorname...">
                                 </div>
                                 <div class="mc-field-group">
-                                    <label for="mce-LNAME">Last Name </label>
-                                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME">
+                                    <!-- <label for="mce-LNAME">Last Name </label> -->
+                                    <input type="text" value="" name="LNAME" class="" id="mce-LNAME" placeholder="Nachname...">
                                 </div>
                                 <div id="mce-responses" class="clear">
                                     <div class="response" id="mce-error-response" style="display:none"></div>
                                     <div class="response" id="mce-success-response" style="display:none"></div>
                                 </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                                 <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_02ed6a6e43772f62f20d35008_a9f47a9457" tabindex="-1" value=""></div>
-                                <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+                                <div class="clear"><input type="submit" value="Jetzt eintragen!" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
                             </div>
                         </form>
                     </div>
