3176c32e618c67ca879241b703b81ee41b3c124d
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-06-13 16:27:10 +0200

    Update

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@
  * functions.php
  */
 
-pll_register_string('optin-headline', 'Not registered yet?', false);
+pll_register_string('optin', 'optin-headline', false);
 pll_register_string('optin-subline', 'Start your live stream in less than 5 minutes', false);
 pll_register_string('optin-register-cta', 'Register now!', false);
 
