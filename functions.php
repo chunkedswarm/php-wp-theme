a499943b88ccb75c4ec6d85a02112619ce370ce3
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-06-13 17:35:30 +0200

    Update

--- functions.php
+++ functions.php
@@ -4,8 +4,28 @@
  * functions.php
  */
 
-pll_register_string('optin', 'optin-headline', false);
-pll_register_string('optin-subline', 'Start your live stream in less than 5 minutes', false);
-pll_register_string('optin-register-cta', 'Register now!', false);
+//////////////////////////////////////////////////////////////////////////////
+//                 Polylang Custom String Translation
+//////////////////////////////////////////////////////////////////////////////
+
+// Top Bar
+pll_register_string('top-bar', '%top-bar-text%', false);
+pll_register_string('top-bar', '%top-bar-register%', false);
+pll_register_string('top-bar', '%top-bar-login%', false);
+
+// Opt-In Register Section
+pll_register_string('optin', '%optin-headline%', false);
+pll_register_string('optin', '%optin-subline%', false);
+pll_register_string('optin', '%optin-register-cta%', false);
+
+// Footer
+pll_register_string('footer', '%footer-headline-product%', false);
+pll_register_string('footer', '%footer-headline-company%', false);
+pll_register_string('footer', '%footer-headline-newsletter%', false);
+// --> Footer weiter customizen
+pll_register_string('footer', '%footer-newsletter-text%', false);
+pll_register_string('footer', '%footer-newsletter-input-placeholder%', false);
+pll_register_string('footer', '%footer-newsletter-input-submit%', false);
+pll_register_string('footer', '%footer-copyright%', false);
 
 ?>
