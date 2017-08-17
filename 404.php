e7d487fb17fbee9329123448bfe4b78a37a62f35
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-08-17 15:43:54 +0200

    Update

--- 404.php
+++ 404.php
@@ -8,7 +8,7 @@ Template Name: 404 - Nicht gefunden
         <div class="_404-container w-container"><img class="_404-image"
                                                      src="<?php echo get_template_directory_uri();?>/images/not-found.png">
             <h1 class="_404-h1">Seite nicht gefunden</h1>
-            <div>Die angeforderte Ressource wurde leider nicht gefunden. XXX</div>
+            <div>Die angeforderte Ressource wurde leider nicht gefunden.</div>
         </div>
     </div>
 <?php get_footer(); ?>
