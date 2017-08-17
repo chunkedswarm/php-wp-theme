45b0fbf66735f963a41dac220be7e156fa388445
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-08-17 15:41:08 +0200

    Update

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ Template Name: 404 - Nicht gefunden
 <?php get_header(); ?>
     <div>
         <div class="_404-container w-container"><img class="_404-image"
-                                                     src="../images/webrtc-logo-vert-retro-255x305.png">
+                                                     src="images/not-found.png">
             <h1 class="_404-h1">Seite nicht gefunden</h1>
             <div>Die angeforderte Ressource wurde leider nicht gefunden. XXX</div>
         </div>
