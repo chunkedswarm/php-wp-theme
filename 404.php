3dd5a0a33117d652aa85556181fa1c7f4c8511f9
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-11-14 15:28:22 +0100

    Update

--- 404.php
+++ 404.php
@@ -1,17 +1,8 @@
 <?php
 /*
-Template Name: 404 - Nicht gefunden
+Template Name: 404 - Not Found
 */
 ?>
 <?php get_header(); ?>
-    <div>
-        <div class="_404-container w-container"><img class="_404-image"
-                                                     src="<?php echo get_template_directory_uri();?>/images/not-found.png">
-            <h1 class="_404-h1">Seite nicht gefunden</h1>
-            <div>Die angeforderte Ressource wurde leider nicht gefunden.</div>
-        </div>
-    </div>
-<script>
-    document.title="404 - Not found";
-</script>
+<?php the_content(); ?>
 <?php get_footer(); ?>
