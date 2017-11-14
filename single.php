bbe2b6dfc1691f7df6f00657fd62ea0dc0534a4c
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-11-14 11:55:30 +0100

    Update

--- single.php
+++ single.php
@@ -1,49 +1,67 @@
 <?php
 /*
-Template Name: Blog Posts
+Template Name: Design V2
 */
 ?>
 <?php
 $template_root = get_template_directory_uri();
-get_header();
 ?>
 
-<div class="section">
-    <div class="container flex-row w-container">
+<!DOCTYPE html>
+<!--  Last Published: Tue Jun 13 2017 12:02:09 GMT+0000 (UTC)  -->
+<html lang="de">
 
-        <!-- BLOG POST CONTAINER -->
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1" name="viewport">
 
-        <div class="blog-container">
+    <title><?php wp_title(); ?></title>
+    <!-- Load favicon and webclip -->
+    <link href="<?php echo $template_root;?>/images/Strive_Logo_Favicon.png" rel="shortcut icon" type="image/x-icon">
+    <link href="<?php echo $template_root;?>/images/Strive_Logo_Webclip.png" rel="apple-touch-icon">
 
-            <h1 class="section-title"><?php the_title(); ?></h1>
-            <div class="blogpost-meta-wrapper">
-                <div class="blogpost-meta-item-wrapper-left">
-                    <img class="blogpost-meta-icon-left"
-                         src="<?php echo $template_root; ?>/images/blogpost-user.png">
-                    <div><?php the_author(); ?></div>
-                </div>
-                <div class="blogpost-meta-item-wrapper-right">
-                    <div><?php the_date(); ?></div>
-                    <img class="blogpost-meta-icon-right"
-                         src="<?php echo $template_root; ?>/images/blogpost-time.png">
-                </div>
-            </div>
-            <div class="v-space-small"></div>
-            <div class="blogpost-image-container single" style="background-image: url(<?php the_field('hero_image'); ?>);"></div>
-            <div class="v-space-small"></div>
-            <div class="justify-text"><strong><?php the_field('sub_headline'); ?></strong></div>
+    <!-- Load CSS files -->
+    <link href="<?php echo $template_root;?>/css/webflow/normalize.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $template_root;?>/css/webflow/webflow.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $template_root;?>/css/webflow/strive-redesign-proposals.webflow.css" rel="stylesheet" type="text/css">
+    <link href="<?php echo $template_root;?>/css/custom_css.css" rel="stylesheet" type="text/css">
 
-            <?php the_content(); ?>
+    <!-- Load Webfont -->
+    <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+    <script type="text/javascript">
+        WebFont.load({
+            google: {
+                families: ["Montserrat:100,100italic,200,200italic,300,300italic,400,400italic,500,500italic,600,600italic,700,700italic,800,800italic,900,900italic","Ubuntu:300,300italic,400,400italic,500,500italic,700,700italic"]
+            }
+        });
+    </script>
 
-        </div>
+    <!-- [if lt IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js" type="text/javascript"></script><![endif] -->
+    <script type="text/javascript">
+        !function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
+    </script>
 
-        <!-- SIDE BAR CONTAINER -->
+    <!-- Load jQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+    <script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css">
+    <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
 
-        <div class="sidebar-container">
-            <?php get_sidebar(); ?>
-        </div>
+    <!-- Start of HubSpot Embed Code -->
+    <!-- <script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/3385585.js"></script> -->
+    <!-- End of HubSpot Embed Code -->
 
-    </div>
-</div>
+    <!-- Load wordpress head -->
+    <?php wp_head(); ?>
 
-<?php get_footer(); ?>
+</head>
+
+<body class="body">
+
+<?php the_content(); ?>
+
+<?php wp_footer(); ?>
+<script src="<?php echo $template_root; ?>/js/webflow.js" type="text/javascript"></script>
+<script src="https://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+</body>
+</html>
